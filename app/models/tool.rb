f39fc19c2496abe6efcd50b060cbd1f8c522b4cd
Ehsan <ehsan@tamhub.com>
2018-07-11 08:21:08 +0300

    Validate tool properties

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,4 +2,8 @@ class Tool < ApplicationRecord
   ## Validation
   validates :name,
             presence: true
+
+  validates :images,
+            presence: true,
+            array_length: { minimum: 1, maximum: 10 }
 end
