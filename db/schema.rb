8cde2f209fa4b9135f1a47e672b6b1cd5b8ec528
Ehsan <ehsan@tamhub.com>
2018-07-11 08:20:34 +0300

    Use postgresql rather than sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_043952) do
+ActiveRecord::Schema.define(version: 2018_07_10_192726) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tools", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "images", array: true
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
