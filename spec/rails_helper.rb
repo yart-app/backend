6f45c7e8faf2dcdca3d25ebbd6d9809389365420
Ehsan <ehsan@tamhub.com>
2018-07-17 14:01:09 +0300

    Load support files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require "shoulda/matchers"
 ENV["RAILS_ENV"] ||= "test"
 
 require File.expand_path("../config/environment", __dir__)
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
