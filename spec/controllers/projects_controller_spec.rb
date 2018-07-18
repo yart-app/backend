9c04554670f263b0735526e79a81c520101bb582
Ehsan <ehsan@tamhub.com>
2018-07-18 13:59:32 +0300

    Assure that categories and statuses are being fetched

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -21,6 +21,14 @@ RSpec.describe ProjectsController, type: :request do
       expect(assigns(:project)).to be_a_new(Project)
     end
 
+    it "assigns the categories to @categories" do
+      expect(assigns(:categories)).to eq(Project::Category::OPTIONS)
+    end
+
+    it "assigns the statuses to @statuses" do
+      expect(assigns(:statuses)).to eq(Project::Status::OPTIONS)
+    end
+
     it "gets all user's projects" do
       user_projects = user.projects.order(created_at: "desc").to_a
       expect(assigns(:projects)).to eql user_projects
