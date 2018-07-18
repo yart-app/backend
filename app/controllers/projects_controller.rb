f74107a087ae46196caba9603b9429315655720a
Ehsan <ehsan@tamhub.com>
2018-07-18 13:53:20 +0300

    Update production database settings

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,9 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.where(user: current_user).order(created_at: "desc").to_a
     @project = Project.new
+    @categories = Project::Category::OPTIONS
+    @statuses = Project::Status::OPTIONS
+  end
+
   end
 end
