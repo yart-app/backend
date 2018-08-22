313d41b542c3ed2d1b7da66397377d02754bef0b
Ehsan <ehsan@tamhub.com>
2018-08-22 17:31:46 +0300

    Add a layer over adding post form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,37 +14,45 @@
 //= require turbolinks
 //= require_tree .
 $(document).on('turbolinks:load', function () {
-    $(".collapsible").click(function () {
-        var content = this.nextElementSibling;
-        var contentToHide = content.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-            contentToHide.style.display = "";
-        } else {
-            content.style.display = "block";
-            contentToHide.style.display = "none";
-        }
-    });
+  $(".collapsible").click(function () {
+    var content = this.nextElementSibling;
+    var contentToHide = content.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      contentToHide.style.display = "";
+    } else {
+      content.style.display = "block";
+      contentToHide.style.display = "none";
+    }
+  });
 
-    // The following code is based off a toggle menu by @Bradcomp
-    // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-    var burger = document.querySelector('.burger');
-    var menu = document.querySelector('#' + burger.dataset.target);
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
+  $("#new-post-card").mouseover(function () {
+    $(".new-post-icon").fadeOut();
+  });
+
+  $("#new-post-card").mouseout(function () {
+    $(".new-post-icon").fadeIn();
+  });
+
+  // The following code is based off a toggle menu by @Bradcomp
+  // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#' + burger.dataset.target);
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  });
 });
 
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('.uploaded-image').attr('src', e.target.result);
-            $('.uploaded-image').removeAttr("style")
-        };
+    reader.onload = function (e) {
+      $('.uploaded-image').attr('src', e.target.result);
+      $('.uploaded-image').removeAttr("style")
+    };
 
-        reader.readAsDataURL(input.files[0]);
-    }
+    reader.readAsDataURL(input.files[0]);
+  }
 }
