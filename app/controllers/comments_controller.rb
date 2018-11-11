5f9d681a9520e9a6f266fcd49a4bc5ddb7f2a73a
Ehsan <ehsan@tamhub.com>
2018-11-11 20:06:02 +0300

    Add an index action to fetch all comments of a specific post / project

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,21 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_page, only: [:index]
+
+  def index
+    @entity = Post.find_by(id: params[:post_id]) ||
+      Project.find_by(id: params[:project_id])
+
+    unless @entity
+      @errors << "Please send either post or project id"
+      render template: "comments/index.json.jbuilder",
+             status: :unprocessable_entity
+      return
+    end
+
+    @comments = @entity.limited_comments(page: @page)
+    render template: "comments/index.json.jbuilder", status: :ok
+  end
 
   def create
     @comment = Comment.add(comment_params, current_user)
