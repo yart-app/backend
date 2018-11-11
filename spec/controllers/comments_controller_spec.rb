50600b61460b18b5b6eab55f64a3eee1c3e64ef2
Ehsan <ehsan@tamhub.com>
2018-11-11 20:20:05 +0300

    Add tests for comments controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,6 +7,72 @@ RSpec.describe CommentsController, type: :request do
     sign_in user
   end
 
+  describe "#index" do
+    describe "when requesting post's comments" do
+      let!(:post) { FactoryBot.create(:post) }
+      let!(:comments) do
+        FactoryBot.create_list(:comment_on_post, 7, post: post)
+      end
+
+      before do
+        get "/comments", params: params
+      end
+
+      context "when not sending page number param" do
+        let(:params) { { post_id: post.id } }
+
+        it "retrieves only 5 comments" do
+          expect(json["comments"].count).to eq(5)
+        end
+
+        it "retrieves the first comments" do
+          json["comments"].each_index do |i|
+            expect(json["comments"][i]["text"]).to eq(comments[i][:text])
+          end
+        end
+      end
+
+      context "when sending page number" do
+        let(:params) { { post_id: post.id, page: 2 } }
+
+        it "retrieves only 2 comments" do
+          expect(json["comments"].count).to eq(2)
+        end
+      end
+    end
+
+    context "when requesting comments without post id nor project id" do
+      let(:params) { { page: 2 } }
+
+      before do
+        get "/comments", params: params
+      end
+
+      it "responds with error status" do
+        expect(response.status).to eq(422)
+      end
+
+      it "responds with error message" do
+        message = "Please send either post or project id"
+        expect(json["errors"]).to include(message)
+      end
+    end
+
+    context "when requesting project's comments" do
+      let!(:project) { FactoryBot.create(:project) }
+      let!(:params) { { project_id: project.id } }
+
+      before do
+        FactoryBot.create_list(:comment_on_project, 4, project: project)
+        get "/comments", params: params
+      end
+
+      it "retrieves all 4 comments" do
+        expect(json["comments"].count).to eq(4)
+      end
+    end
+  end
+
   describe "#create" do
 
     let(:params) do
@@ -14,7 +80,6 @@ RSpec.describe CommentsController, type: :request do
         comment: {
           post_id: comment.post_id,
           project_id: comment.project_id,
-          user_id: comment.user_id,
           text: comment.text
         }
       }
