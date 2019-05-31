1cc183f047514787938f85fab2f6b42822bfed34
Ehsan Zumrut <dev.e7san@gmail.com>
2019-05-31 03:43:50 +0300

    Get only undone projects for the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
 
   def index
     @post = Post.new
-    @projects = current_user.ordered_projects.to_a
+
+    ordered_projects = current_user.ordered_projects
+
+    @projects = current_user.undone_projects(ordered_projects).to_a
     @posts = current_user.ordered_posts_with_friends_posts.to_a
   end
 end
