b3f5680500121db827d22e0e8ee5100ed3f50e8b
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-04 01:09:12 +0300

    Move posts js to the proper file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,34 +27,3 @@ $(document).on('turbolinks:load', function () {
     menu.classList.toggle('is-active');
   });
 });
-
-function readImageURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function (e) {
-      $('.uploaded-image').attr('src', e.target.result);
-      $('.uploaded-image').removeAttr("style");
-    };
-
-    reader.readAsDataURL(input.files[0]);
-    runIntoJsOnSubmitBtn();
-  }
-}
-
-function runIntoJsOnSubmitBtn() {
-  const onboarded = $('.onboarded').val();
-
-  if (onboarded == "false") {
-    // Use setTimeOut here to wait for dom to update before
-    // starting introJs and focus on submit button
-
-    setTimeout(() => {
-      introJs().setOptions({
-        'scrollTo': $('.submit'),
-      });
-
-      introJs().goToStepNumber(5).start();
-    }, 50);
-  }
-}
