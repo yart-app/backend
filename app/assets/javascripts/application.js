22969f40ce59f83262a07c596fdc19829843b1ed
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-04 01:09:27 +0300

    Use const instead of var

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,9 @@ $(document).on('turbolinks:load', function () {
 
   // The following code is based off a toggle menu by @Bradcomp
   // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-  var burger = document.querySelector('.burger');
-  var menu = document.querySelector('#' + burger.dataset.target);
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('#' + burger.dataset.target);
+
   burger.addEventListener('click', function () {
     burger.classList.toggle('is-active');
     menu.classList.toggle('is-active');
