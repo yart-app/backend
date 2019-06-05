dfee9ef882cd48a0a29522bc84328d9f99a649df
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-05 09:45:13 +0300

    Apply select2 with create option on project deopdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,9 @@
 
 "use strict";
 $(document).on('turbolinks:load', function () {
+  $('.select2-with-create-option').select2({
+    tags: true
+  });
 
   // The following code is based off a toggle menu by @Bradcomp
   // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
