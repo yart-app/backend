92c8fcde49b630b8f3a0488b605962c9620ccceb
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-14 20:57:24 +0300

    Revert "Fix issue with yart.app"
    This reverts commit 436dec3952cc1ff079c7f65e0622de6a168626f3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :reset_session
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   respond_to :html, :json
