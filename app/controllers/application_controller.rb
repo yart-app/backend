436dec3952cc1ff079c7f65e0622de6a168626f3
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-14 20:51:54 +0300

    Fix issue with yart.app
    The error: ActionController::InvalidAuthenticityToken
    
    The solution source:
    https://stackoverflow.com/questions/22561733/rails-actioncontrollerinvalidauthenticitytoken-error-heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :reset_session
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   respond_to :html, :json
