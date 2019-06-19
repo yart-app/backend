38185c5b6f548e0608902f22f8458bf333883b5f
Ehsan Zumrut <dev.e7san@gmail.com>
2019-06-20 00:34:14 +0300

    Add a method to update timeline on updating a field

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -1,6 +1,24 @@
 "use strict";
 
+function updateTimeline(post) {
+  var timelineContainer = $('.timeline');
 
+  var newPostElement =
+    '<div class="timeline-item">' +
+    '<div class="timeline-marker is-icon">' +
+    '<i class="fa fa-arrow-circle-right"></i>' +
+    '</div>' +
+    '<div class="timeline-content">' +
+    '<p class="heading">' +
+    post.created_at +
+    '</p>' +
+    '<p class="has-text-grey-light">' +
+    post.text +
+    '</p>' +
+    '</div>' +
+    '</div';
+
+  timelineContainer.append(newPostElement);
 }
 
 function update_by_field(id, key, value) {
