65850c6173899a495a642f44a32b703ac22a5ca0
Ehsan Zumrut <dev.e7san@gmail.com>
2019-08-31 11:49:19 +0300

    Add manifest file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.configuration.assets.precompile += %w[
   serviceworker.js
+  manifest.json
 ]
