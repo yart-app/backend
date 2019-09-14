f96a4c5f0e554f5ab021254d539468680b77ed5a
EhsanZ <dev.e7san@gmail.com>
2019-09-14 19:25:41 +0300

    Separate like request in its own js file

---
+++ app/assets/javascripts/like.js
@@ -0,0 +1,20 @@
+"use strict";
+$(document).on('turbolinks:load', function () {
+  $('.like').click(function (e) {
+    var data = {
+      'id': e.target.id,
+    };
+
+    $.post('/posts/likes', data, function (response, status) {
+      if (status !== 422) {
+        var icon = 'favorite_border';
+
+        if (response.message === "liked") {
+          icon = 'favorite';
+        }
+        $('#likes-size-' + data.id).html(response.likes_size + " Likes");
+        $(e.target).html(icon);
+      }
+    });
+  });
+});
