4fe0ed7d30e3355b68e51e2a3c1fa8d2d25bedf0
EhsanZ <dev.e7san@gmail.com>
2019-09-14 19:23:40 +0300

    Activate bulma tabs from js & css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,13 +40,27 @@ $(document).on('turbolinks:load', function () {
     tags: true
   });
 
+  $('.tabs').each(function (index) {
+    var $tabParent = $(this);
+    var $tabs = $tabParent.find('li');
+    var $contents = $tabParent.next('.tabs-content').find('.tab-content');
 
+    $tabs.click(function () {
+      var curIndex = $(this).index();
+      // toggle tabs
+      $tabs.removeClass('is-active');
+      $tabs.eq(curIndex).addClass('is-active');
+      // toggle contents
+      $contents.removeClass('is-active');
+      $contents.eq(curIndex).addClass('is-active');
+    });
   });
 });
 
+
 // credits: https://github.com/turbolinks/turbolinks/issues/17#issuecomment-230169838
-Turbolinks.ProgressBar.prototype.refresh = function () {}
-Turbolinks.ProgressBar.defaultCSS = ""
+Turbolinks.ProgressBar.prototype.refresh = function () {};
+Turbolinks.ProgressBar.defaultCSS = '';
 
 Turbolinks.ProgressBar.prototype.uninstallProgressElement = function () {
   $(document).find('.wait, .overlay').remove();
